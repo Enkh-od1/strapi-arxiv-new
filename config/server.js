d570897ce276de35439c135be21a1d783c609927
unknown <enkh-od@naog.gov.mn>
2026-04-15 12:43:59 +0800

    Disable Strapi telemetry to fix 401 error

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // Доорх мөрийг нэмнэ:
+  url: env('STRAPI_URL', 'https://jpa.naog.edu.mn'), 
   app: {
     keys: env.array('APP_KEYS'),
   },
